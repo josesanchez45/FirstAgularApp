7c4c8155d05ace037881187fbe81e8c04b2aad9d
jose sanchez <jose.sanchez.artist@gmail.com>
2022-09-19 15:34:44 -0700

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'first-angular-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('first-angular-app');
+    expect(app.name).toEqual('first-angular-app');
   });
 
   it('should render title', () => {
